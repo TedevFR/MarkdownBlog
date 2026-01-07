58e1440d84286972f55bbf15a81679184153c83f
Teddy LE BRAS <teddy.lebras@gmail.com>
2026-01-07 22:40:30 +0100

    Update current directory retrieval in TryParseArgs

--- generator/src/MarkdownBlog.Generator/Program.cs
+++ generator/src/MarkdownBlog.Generator/Program.cs
@@ -52,7 +52,7 @@ internal static class Program
 
     private static bool TryParseArgs(string[] args, out GeneratorOptions options, out string error)
     {
-        var cwd = Directory.GetCurrentDirectory();
+        var cwd = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
         string? input = Path.Combine(cwd, "content");
         string? templates = Path.Combine(cwd, "site", "templates");
         string? assets = Path.Combine(cwd, "site", "assets");
